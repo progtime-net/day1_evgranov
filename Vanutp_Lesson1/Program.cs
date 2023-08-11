102fa560a461ff8c9f27a29d40105ec248819e96
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2023-08-11 18:02:07 +0300

    Commit Write Name

--- Vanutp_Lesson1/Program.cs
+++ Vanutp_Lesson1/Program.cs
@@ -4,6 +4,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Привет, как тебя зовут?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}"); 
     }
 }
