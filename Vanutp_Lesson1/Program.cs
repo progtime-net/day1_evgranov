a60d3f8b2028e16ae7bf12bb71509249e8c4b7a5
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2023-08-11 18:18:45 +0300

    Added Age

--- Vanutp_Lesson1/Program.cs
+++ Vanutp_Lesson1/Program.cs
@@ -6,6 +6,11 @@ internal class Program
     {
         Console.WriteLine("Привет, как тебя зовут?");
         string name = Console.ReadLine();
-        Console.WriteLine($"Привет, {name}"); 
+        Console.WriteLine($"Привет, {name}");
+
+        Console.WriteLine("Введите ваш возраст");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Ваш возраст через 10 лет: {age + 10}");
     }
 }
